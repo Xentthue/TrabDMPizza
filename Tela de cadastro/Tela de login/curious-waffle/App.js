99c3b0dbcfb27c6ec781610bd54d65cd4e71dfc9
PRobertoSilva <64875229+PRobertoSilva@users.noreply.github.com>
2020-06-09 18:21:59 -0300

    Add files via upload

---
+++ Tela de cadastro/Tela de login/curious-waffle/App.js
@@ -0,0 +1,76 @@
+import * as React from 'react';
+import { Text, View, StyleSheet, Image, TextInput, TouchableOpacity } from 'react-native';
+
+export default function AssetExample() {
+
+  return (
+    <View style={styles.container}>
+      <Image style={styles.logo}
+            source={{
+              uri:
+                'https://i.imgur.com/BP7N5CY.png',
+            }}
+          />
+      <TextInput
+      style={styles.input}
+      Placeholder="Digite seu Email"
+      /> 
+      <TextInput
+      style={styles.input}
+      secureTextEntry={true}
+      Placeholder="Digite sua Senha"
+      /> 
+      <TouchableOpacity
+      style={styles.botao}
+      onPress={ () => {this.clicou()} }
+      >
+      <Text style={styles.botaoText}> Login </Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+      style={styles.botao}
+      onPress={ () => {this.clicou()} }
+      >
+      <Text style={styles.botaoText}> Criar Conta </Text>
+      </TouchableOpacity>
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#000000'
+  },
+  logo: {
+    height: 150,
+    width: 150,
+    borderRadius: 100
+  },
+  input:{
+    marginTop: 10,
+    padding: 10,
+    width: 300,
+    backgroundColor: '#FFF',
+    fontsize: 16,
+    fontweight: 'bold',
+    borderRadius: 3
+  },
+  botao:{
+    width: 300,
+    height: 42,
+    backgroundColor: '#fcffff',
+    marginTop: 10,
+    borderRadius: 4,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  botaoText:{
+    fontsize: 16,
+    fontweight: 'bold',
+    color: '#000000'
+  }
+});
